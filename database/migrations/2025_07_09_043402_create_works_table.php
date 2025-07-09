eeb672cadb080d1ca304e6f13162a268affc7d88
kouheigit <ookawakouhei777@gmail.com>
2025-07-09 14:09:57 +0900

    feat: Worksテーブル作成のマイグレーションを実行

--- database/migrations/2025_07_09_043402_create_works_table.php
+++ database/migrations/2025_07_09_043402_create_works_table.php
@@ -12,8 +12,15 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('works', function (Blueprint $table) {
-            $table->id();
+            $table->bigIncrements('id');
+            $table->string('title');
+            $table->text('content');
+            $table->string('status');
+            $table->date('due_date');
+            $table->foreignId('category_id')->nullable()->constrained()->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
