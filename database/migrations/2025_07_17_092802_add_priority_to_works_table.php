1ac2e40781c41e2f9807a3bc3c7fde577530bf22
kouheigit <ookawakouhei777@gmail.com>
2025-07-17 20:26:13 +0900

    Add worksテーブルにpriorityカラム（初期値3）を追加

---
+++ database/migrations/2025_07_17_092802_add_priority_to_works_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('works', function (Blueprint $table) {
+            $table->unsignedTinyInteger('priority')->default(3)->after('status');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('works', function (Blueprint $table) {
+            $table->dropColumn('priority');
+        });
+    }
+};
+
+
