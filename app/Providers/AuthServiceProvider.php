ef3a6d33a615d4c29d2b16f2821bc92b6adb1c74
kouheigit <ookawakouhei777@gmail.com>
2025-08-01 21:40:54 +0900

    feat: TaskItemモデルに対するポリシーをAuthServiceProviderに登録

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,17 +2,16 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\ServiceProvider;
+use App\Models\TaskItem;
+use App\Policies\TaskItemPolicy;
+use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     */
-    public function register(): void
-    {
-        //
-    }
+
+    protected $policies = [
+      TaskItem::class=>TaskItemPolicy::class,
+    ];
 
     /**
      * Bootstrap services.
