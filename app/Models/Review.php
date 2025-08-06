0db10ed3a32db5ca7b5ec43890a8fced60cba801
kouheigit <ookawakouhei777@gmail.com>
2025-08-06 11:48:17 +0900

    feat: ProductとReview間のリレーション（1対多）を定義

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -3,8 +3,10 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\Models\Product;
 class Review extends Model
 {
-    //
+    public function products(){
+        return $this->belongsTo(Product::class);
+    }
 }
