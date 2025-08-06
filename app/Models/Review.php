19887672521a54e6796c9903e81c624ec1ca6400
kouheigit <ookawakouhei777@gmail.com>
2025-08-06 12:30:31 +0900

    feat: Reviewモデルにカラム定義とProductとのリレーションを追加

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -6,6 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 use App\Models\Product;
 class Review extends Model
 {
+    protected $fillable = [
+        'title',
+        'content',
+        'status',
+        'due_date',
+        'priority',
+        'product_id'
+    ];
     public function products(){
         return $this->belongsTo(Product::class);
     }
