7a4e6945abe808efc9cf8a3131926b377395e78e
kouheigit <ookawakouhei777@gmail.com>
2025-10-02 08:59:33 +0900

    Add:TaskNoteモデルの新規追加

--- app/Http/Resources/TaskNoteResource.php
+++ app/Http/Resources/TaskNoteResource.php
@@ -21,22 +21,12 @@ class TaskNoteResource extends JsonResource
             'status'=>$this->status,
             'is_overdue'=>$this->is_overdue,
             'due_date' => $this->due_date->toDateString(),
+            'priority'=>$this->priority,
+            'group'=>$this->group?->name,
+            'user'=>$this->user->name,
+            'created_at' => $this->created_at->format('Y-m-d H:i:s'),
 
-          /*
-           *    'id' => $this->id,
-        'title' => $this->title,
-        'content' => $this->content,
-        'status' => $this->status,                       // NoteStatus の文字列表現
-        'is_overdue' => $this->is_overdue,               // アクセサ
-        'due_date' => $this->due_date->toDateString(),
-        'priority' => $this->priority,
-        'group' => $this->group?->name,
-        'user' => $this->user->name,
-        'created_at' => $this->created_at->format('Y-m-d H:i:s'),
-           */
         ];
-        /*
-        return parent::toArray($request);
-        */
+
     }
 }
