32f252bdff47eb8aff20d8961ed2b56fd8932dfe
kouheigit <ookawakouhei777@gmail.com>
2025-10-01 18:54:02 +0900

    feat:TaskGroupの実装

--- app/Models/TaskGroup.php
+++ app/Models/TaskGroup.php
@@ -3,8 +3,17 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class TaskGroup extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['name'];
+
+    public function taskNotes()
+    {
+        return $this->hasMany(TaskNote::class,'task_group_id');
+    }
 }
+
+
