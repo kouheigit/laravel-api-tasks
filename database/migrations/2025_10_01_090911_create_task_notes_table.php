d7d2fff4ecbf14b4c89879a9e516b0a0b93b0ad1
kouheigit <ookawakouhei777@gmail.com>
2025-10-01 19:27:47 +0900

    feat:migrationファイルの実装

--- database/migrations/2025_10_01_090911_create_task_notes_table.php
+++ database/migrations/2025_10_01_090911_create_task_notes_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('task_notes', function (Blueprint $table) {
             $table->id();
+            $table->string('title');
+            $table->text('content');
+            $table->string('status');
+            $table->date('due_date');
+            $table->tinyInteger('priority')->default(3);
+            $table->foreignId('task_group_id')->nullable()
+                ->constrained('task_groups')->nullOnDelete();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
+            $table->timestamps();
+            $table->softDeletes();
+            $table->unique(['user_id', 'title']);
             $table->timestamps();
         });
     }
