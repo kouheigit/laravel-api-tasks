f0fe5999556af3b7e74a301f90312115f030745c
kouheigit <ookawakouhei777@gmail.com>
2025-10-01 21:12:37 +0900

    feat:TaskNoteResourceの実装

--- app/Http/Resources/TaskNoteResource.php
+++ app/Http/Resources/TaskNoteResource.php
@@ -14,6 +14,29 @@ class TaskNoteResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+        return [
+            'id' =>$this->id,
+            'title'=>$this->title,
+            'content'=>$this->content,
+            'status'=>$this->status,
+            'is_overdue'=>$this->is_overdue,
+            'due_date' => $this->due_date->toDateString(),
+
+          /*
+           *    'id' => $this->id,
+        'title' => $this->title,
+        'content' => $this->content,
+        'status' => $this->status,                       // NoteStatus の文字列表現
+        'is_overdue' => $this->is_overdue,               // アクセサ
+        'due_date' => $this->due_date->toDateString(),
+        'priority' => $this->priority,
+        'group' => $this->group?->name,
+        'user' => $this->user->name,
+        'created_at' => $this->created_at->format('Y-m-d H:i:s'),
+           */
+        ];
+        /*
         return parent::toArray($request);
+        */
     }
 }
