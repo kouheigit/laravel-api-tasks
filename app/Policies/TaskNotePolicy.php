fabf835d74354f3a6a4f735fcfb7a24d113eaf94
kouheigit <ookawakouhei777@gmail.com>
2025-10-02 09:13:50 +0900

    feat:TaskNotepolicyの実装

--- app/Policies/TaskNotePolicy.php
+++ app/Policies/TaskNotePolicy.php
@@ -11,56 +11,33 @@ class TaskNotePolicy
     /**
      * Determine whether the user can view any models.
      */
-    public function viewAny(User $user): bool
-    {
-        return false;
-    }
-
     /**
      * Determine whether the user can view the model.
      */
-    public function view(User $user, TaskNote $taskNote): bool
+    public function view(User $user, TaskNote $note): bool
     {
-        return false;
+        return $note->user_id === $user->id;
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
-    public function create(User $user): bool
-    {
-        return false;
-    }
 
     /**
      * Determine whether the user can update the model.
      */
-    public function update(User $user, TaskNote $taskNote): bool
+    public function update(User $user, TaskNote $note): bool
     {
-        return false;
+        return $note->user_id === $user->id;
     }
 
     /**
      * Determine whether the user can delete the model.
      */
-    public function delete(User $user, TaskNote $taskNote): bool
+    public function delete(User $user, TaskNote $note): bool
     {
-        return false;
+        return $note->user_id === $user->id;
+    }
+    public function create(User $user): bool
+    {
+        return true;
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
-    public function restore(User $user, TaskNote $taskNote): bool
-    {
-        return false;
-    }
-
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
-    public function forceDelete(User $user, TaskNote $taskNote): bool
-    {
-        return false;
-    }
 }
