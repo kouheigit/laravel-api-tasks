0be2584ce6493fbf0391e539e964fe751ca2ac6b
kouheigit <ookawakouhei777@gmail.com>
2025-10-08 17:18:18 +0900

    feat:Writerモデルへの実装

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Article extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['title','body','writer_id'];
+    public function writer()
+    {
+        return $this->belongsTo(Writer::class);
+    }
 }
