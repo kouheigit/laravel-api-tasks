b6ed373d935b450f814cce6f4c4996b0dc290702
kouheigit <ookawakouhei777@gmail.com>
2025-10-08 17:08:37 +0900

    feat:Writerモデルへの実装

--- app/Models/Writer.php
+++ app/Models/Writer.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Writer extends Model
 {
-    //
+    use HasFactory,HasApiTokens;
+
+    protected $fillable = ['name','email','password'];
+
+    public function articles()
+    {
+        return $this->hasMany(Article::class);
+    }
 }
