a67b26c04a38023bd9e2719b07f5b86869cb4736
kouheigit <ookawakouhei777@gmail.com>
2025-10-11 16:35:51 +0900

    feat:AuthServiceProviderへのArticleModel&Policyの登録

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -5,9 +5,11 @@ namespace App\Providers;
 use App\Models\TaskItem;
 use App\Models\TaskNote;
 use App\Models\Review;
+use App\Models\Article;
 use App\Policies\ReviewPolicy;
 use App\Policies\TaskItemPolicy;
 use App\Policies\TaskNotePolicy;
+use App\Policies\ArticlePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 
 class AuthServiceProvider extends ServiceProvider
@@ -17,6 +19,7 @@ class AuthServiceProvider extends ServiceProvider
         TaskNote::class => TaskNotePolicy::class,
         TaskItem::class=>TaskItemPolicy::class,
         Review::class=>ReviewPolicy::class,
+        Article::class =>ArticlePolicy::class,
     ];
 
     /**
