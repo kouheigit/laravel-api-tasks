6a7735ac8d11eeab213809552e73b28d395386a4
kouheigit <ookawakouhei777@gmail.com>
2025-10-12 13:54:47 +0900

    feat:Level3.jsxの実装

--- React-view/src/training/Level3.jsx
+++ React-view/src/training/Level3.jsx
@@ -1,13 +1,27 @@
 import React,{　useState　} from 'react';
 function Level3(){
-    
+
     const[todos,setTodos] = useState([]);
     const[inputs,setInputs] = useState('');
     const[filter,setFilter] = useState('all');
 
+    const addTodo = () =>{
+        if(input.trim()==='')retrun;
+        setTodos([...todos,{text:input,completed:false}]);
+        setInputs('');
+    }
+    /*
+     const toggleComplete = (index) => {
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    };
+     */
     return (
         <div>
 
+            <input type ="text" value={input} onChange={(e)=>setInput(e.target.value)}/>
+            <button onClick={addTodo}>追加</button>
         </div>
     );
 }
