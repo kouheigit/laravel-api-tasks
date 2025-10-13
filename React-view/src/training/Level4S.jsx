c8677949ac4c07a1f71433d8fe9810b0873076ff
kouheigit <ookawakouhei777@gmail.com>
2025-10-13 18:28:07 +0900

    feat:level4Sの実装

--- React-view/src/training/Level4S.jsx
+++ React-view/src/training/Level4S.jsx
@@ -1,35 +1,30 @@
-import React, { useState } from 'react';
-
+import React,{ useState } from 'react';
 function Level4(){
-    const [count,setCount] = useState(0);
-    const [history,setHistory] = useState([]);
+    const[count,setCount] = useState(0);
+    const[history,setHistory] = useState([]);
 
-    const increment = () =>{
+    const increment = () => {
         setCount(count + 1);
-        setHistory([...history, `+1: ${count} → ${count + 1}`]);
-
+        setHistory([...history,`+1:${count}→${count + 1}`]);
     }
-
     const decrement = () =>{
         setCount(count - 1);
-        setHistory([...history,`+1:${count}→${count - 1}`]);
-    }
+        setHistory([...history,`-1:${count}→${count + 1}`]);
 
+    }
     const reset = () =>{
         setCount(0);
-        setHistory([...history,`リセット:${count} → 0`]);
+        setHistory([...history,`リセット:${count}→0`]);
     }
-
     const clearHistory = () =>{
         setHistory([]);
     }
-    return (
+    return(
         <div>
             <h1>現在の値{count}</h1>
             <button onClick={increment}>+1</button>
             <button onClick={decrement}>-1</button>
             <button onClick={reset}>リセット</button>
-            <h3>履歴</h3>
             <button onClick={clearHistory}>履歴をクリア</button>
             <ul>
                 {history.map((item,index)=>(
@@ -39,6 +34,3 @@ function Level4(){
         </div>
     );
 }
-
-
-export default Level4;
