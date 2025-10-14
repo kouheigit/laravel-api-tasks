7a94c7c409e0ee4749bb512978aba3014503da15
kouheigit <ookawakouhei777@gmail.com>
2025-10-14 20:16:13 +0900

    feat:PostcardModel

--- app/Models/Postcard.php
+++ app/Models/Postcard.php
@@ -3,8 +3,14 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Postcard extends Model
 {
-    //
+    use HasFactory;
+    protected $fillable = ['headline','message','scribe_account_id'];
+    public function scribeAccount()
+    {
+        return $this->belongsTo(ScribeAccount::class);
+    }
+
 }
--- app/Models/ScribeAccount.php
+++ app/Models/ScribeAccount.php
@@ -3,8 +3,18 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Sanctum\HasApiTokens;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class ScribeAccount extends Model
 {
-    //
+    use HasApiTokens;
+    use HasFactory;
+
+    protected $fillable = ['name','email','password'];
+
+    public function postcards()
+    {
+        return $this->hasMany(Postcard::class);
+    }
 }
