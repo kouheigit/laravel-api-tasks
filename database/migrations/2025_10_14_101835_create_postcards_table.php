4554e88b959afed416b56df2e4c45ace4623af38
kouheigit <ookawakouhei777@gmail.com>
2025-10-14 19:41:54 +0900

    feat:create_postcart_tableの実装

--- database/migrations/2025_10_14_101835_create_postcards_table.php
+++ database/migrations/2025_10_14_101835_create_postcards_table.php
@@ -13,7 +13,9 @@ return new class extends Migration
     {
         Schema::create('postcards', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('headline',150);
+            $table->taxt('message');
+            $table->foreignId('scribe_account_id')->constrained()->cascadeOnDelete();
         });
     }
 
