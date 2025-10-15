7b023cf91f36be8ffe010f0d15a3088672917166
kouheigit <ookawakouhei777@gmail.com>
2025-10-15 15:04:45 +0900

    feat:PostcardControllerV2の実装

--- app/Http/Controllers/PostcardControllerV2.php
+++ app/Http/Controllers/PostcardControllerV2.php
@@ -2,7 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Postcard;
+use App\Http\Requests\StorePostcardRequestV2;
+use App\Http\Requests\UpdatePostcardRequestV2;
+use App\Http\Resources\PostcardResourceV2;
+use App\Policies\PostcardPolicyV2;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
+
 
 class PostcardControllerV2 extends Controller
 {
@@ -11,7 +18,10 @@ class PostcardControllerV2 extends Controller
      */
     public function index()
     {
-        //
+        return PostcardResourceV2::collection(
+            Postcard::with('scribeAccount')->lates()->paginate(10)
+        );
+
     }
 
     /**
@@ -19,30 +29,35 @@ class PostcardControllerV2 extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $postcard = Auth::user()->postcards()->create($request->validated());
+        return (new PostcardPolicyV2($postcard))->response()->setStatusCode(201);
     }
 
     /**
      * Display the specified resource.
      */
-    public function show(string $id)
+    public function show(Postcard $postcard)
     {
-        //
+        return new PostcardResourceV2($postcard->load('scribeAccount'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdatePostcardRequestV2 $request, Postcard $postcard)
     {
-        //
+        $this->authorize('update',$postcard);
+        $postcard->update($request->validated());
+        return new　PostcardResourceV2($postcard);
     }
 
     /**
      * Remove the specified resource from storage.
      */
-    public function destroy(string $id)
+    public function destroy(Postcard $postcard)
     {
-        //
+        $this->authorize('delete',$postcard);
+        $postcard->delete();
+        return response()->json(['message' => 'Deleted']);
     }
 }
