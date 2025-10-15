b0cf5e08d83a9cd38cea6a73b04f098ebc72c121
kouheigit <ookawakouhei777@gmail.com>
2025-10-15 12:53:36 +0900

    feat: StorePostcardRequestV2の実装

--- app/Http/Requests/StorePostcardRequestV2.php
+++ app/Http/Requests/StorePostcardRequestV2.php
@@ -11,7 +11,7 @@ class StorePostcardRequestV2 extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,8 @@ class StorePostcardRequestV2 extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'headline'=>['required','string','max:150'],
+            'message'=>['required','string','max:1000'],
         ];
     }
 }
