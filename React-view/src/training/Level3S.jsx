5b942502b0a33b712b14130349fe8ab07c2173e3
kouheigit <ookawakouhei777@gmail.com>
2025-10-16 18:37:23 +0900

    feat:Level3S.jsxの実装

--- React-view/src/training/Level3S.jsx
+++ React-view/src/training/Level3S.jsx
@@ -1,7 +1,9 @@
 import React,{ useState } from 'react';
 
 function Level3S(){
-
+    const[todos,setTodos] = useState([]);
+    const[inputs,setInputs] = useState('');
+    const[filters,setFilter] = useState('all');
 
     return view(
         <div>
