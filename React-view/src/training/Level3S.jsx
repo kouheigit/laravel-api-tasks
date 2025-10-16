c9ec898b6f693e67b028c8ff17360c59cecddda3
kouheigit <ookawakouhei777@gmail.com>
2025-10-16 19:18:55 +0900

    feat:Level3Sの実装

--- React-view/src/training/Level3S.jsx
+++ React-view/src/training/Level3S.jsx
@@ -5,8 +5,29 @@ function Level3S(){
     const[inputs,setInputs] = useState('');
     const[filters,setFilter] = useState('all');
 
+    const addTodo = () =>{
+        if(inputs.trim==='')return;
+        setTodos([...todos,{text:inputs,completed:false}]);
+        setInputs('');
+
+    }
+    const toggleComplete = () =>{
+        const newTodos = [...todos];
+        newTodos[index].completed = !newTodos[index].completed;
+        setTodos(newTodos);
+    }
+
+    const deleteTodo = () =>{
+        if(filter ==='active') return!todo.completed;
+        if(filter ==='completed') return todo.completed;
+        return true;
+    }
+
     return view(
         <div>
+            <inoput type="text" value={input} onChange{(e)=>setInputs(e.target.value)}/>
+            <button onClick={addTodo}>追加</button>
+
 
         </div>
     );
