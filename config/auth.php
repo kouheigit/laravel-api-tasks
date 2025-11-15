4690b630c4a3cb27ffb54b3dacc6f0d298eb286a
kouheigit <ookawakouhei777@gmail.com>
2025-11-15 21:43:49 +0900

    feat:guardの追加

--- config/auth.php
+++ config/auth.php
@@ -40,6 +40,7 @@ return [
             'driver' => 'session',
             'provider' => 'users',
         ],
+        //todo
         'todo'=>[
           'driver'=>'session',
           'provider'=>'todousers',
