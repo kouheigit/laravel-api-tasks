a9b4fffa09e37a2d339fc4792598aebf05201aeb
kouheigit <ookawakouhei777@gmail.com>
2025-11-15 22:03:28 +0900

    feat:TodoAuthControllerの実装

--- app/Http/Controllers/TodoAuthController.php
+++ app/Http/Controllers/TodoAuthController.php
@@ -3,8 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class TodoAuthController extends Controller
 {
-    //
+    public function showLoginForm()
+    {
+        return view('todo.login');
+    }
+  
 }
