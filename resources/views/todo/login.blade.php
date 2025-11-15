e1e8cb196a6dae63bf53d46edef15085765d759b
kouheigit <ookawakouhei777@gmail.com>
2025-11-15 23:19:21 +0900

    feat:loginブレード

--- resources/views/todo/login.blade.php
+++ resources/views/todo/login.blade.php
@@ -17,46 +17,16 @@
 @endif
 <form method="POST" action="{{route('todo.login')}}">
     @csrf
-
-    
-</form>
-
-</body>
-</html>
-{{--
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>TodoUser Login</title>
-</head>
-<body>
-<h2>TodoUser ログイン</h2>
-
-@if ($errors->any())
     <div>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li style="color:red">{{ $error }}</li>
-            @endforeach
-        </ul>
+        <label>Email:</label>
+        <input type="email" name="email" value="{{old('email')}}">
     </div>
-@endif
-
-<form method="POST" action="{{ route('todo.login') }}">
-    @csrf
-
     <div>
-        <label>Email：</label>
-        <input type="email" name="email" value="{{ old('email') }}">
-    </div>
-
-    <div>
-        <label>Password：</label>
+        <label>Password:</label>
         <input type="password" name="password">
     </div>
-
     <button type="submit">ログイン</button>
 </form>
 </body>
-</html>--}}
+</html>
+
