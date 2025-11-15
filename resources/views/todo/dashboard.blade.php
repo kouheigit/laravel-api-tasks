6b9b26540ee34a60bb12dac02d2bb5b70613a460
kouheigit <ookawakouhei777@gmail.com>
2025-11-15 23:26:04 +0900

    feat:dashbord.blade.phpの実装

---
+++ resources/views/todo/dashboard.blade.php
@@ -0,0 +1,8 @@
+<h1>TodoUser　ダッシュボード</h1>
+<p>
+    {{Auth::guard('todo')->user()->name }}さんでログイン中
+</p>
+<form method="POST" action="{{route('todo.logout')}}">
+    @csrf
+    <button type="submit">ログアウト</button>
+</form>
