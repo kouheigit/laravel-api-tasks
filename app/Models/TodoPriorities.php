6e1a41269c1288e5890079d24b2a8f14f22a4530
kouheigit <ookawakouhei777@gmail.com>
2025-11-17 20:07:58 +0900

    feat:TodoPrioritiesの実装

--- app/Models/TodoPriorities.php
+++ app/Models/TodoPriorities.php
@@ -6,5 +6,16 @@ use Illuminate\Database\Eloquent\Model;
 
 class TodoPriorities extends Model
 {
-    //
+    protected $table = 'todo_priorities';
+
+    protected $fillable =[
+        'name',
+        'label',
+        'sort_order',
+    ];
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(TodoTask::class, 'todo_priority_id');
+    }
+    
 }
