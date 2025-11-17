fab1d090c87c15376a1682cb6dbc173865904bae
kouheigit <ookawakouhei777@gmail.com>
2025-11-17 20:31:22 +0900

    feat:TodoTaskモデルへの追記

--- app/Models/TodoTasks.php
+++ app/Models/TodoTasks.php
@@ -7,7 +7,14 @@ use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TodoTasks extends Model
 {
-    /*
-     *
-     */
+    protected $table = 'todo_tasks';
+
+    protected $fillable = [
+        'todo_user_id',
+        'title',
+        'description',
+        'due_date',
+
+    ];
+   
 }
