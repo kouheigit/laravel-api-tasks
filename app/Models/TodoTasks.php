a855a8524d66383a8223c6bf87465d27ec289f5d
kouheigit <ookawakouhei777@gmail.com>
2025-11-17 20:38:41 +0900

    feat:TodoTaskモデルの実装

--- app/Models/TodoTasks.php
+++ app/Models/TodoTasks.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class TodoTasks extends Model
@@ -14,7 +15,25 @@ class TodoTasks extends Model
         'title',
         'description',
         'due_date',
-
+        'todo_status_id',
+        'todo_priority_id',
     ];
-   
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(TodoUser::class, 'todo_user_id');
+    }
+
+
+    public function status(): BelongsTo
+    {
+        return $this->belongsTo(TodoStatus::class, 'todo_status_id');
+    }
+
+
+    public function priority(): BelongsTo
+    {
+        return $this->belongsTo(TodoPriority::class, 'todo_priority_id');
+    }
+    
 }
