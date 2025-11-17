c9f9f1faf0cbd76d6a03c4c85c5a365abaf81c61
kouheigit <ookawakouhei777@gmail.com>
2025-11-17 20:00:59 +0900

    feat:TodoStatusの実装

--- app/Models/TodoStatus.php
+++ app/Models/TodoStatus.php
@@ -6,5 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class TodoStatus extends Model
 {
-    //
+    protected $table = 'todo_statuses';
+
+    protected $fillable = [
+      'name',
+      'label',
+    ];
+
+    public function tasks(): HasMany
+    {
+        return $this->hasMany(TodoTask::class,'todo_status_id');
+    }
 }
