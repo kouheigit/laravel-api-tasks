62f6e79fb0308183e7477f9454e5e903a064fa3e
kouheigit <ookawakouhei777@gmail.com>
2025-11-18 18:37:24 +0900

    feat:index.blade.phpの実装

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -3,7 +3,17 @@
 
     <h1>test</h1>
     <h1>Todoインデックスブレード</h1>
-    <h1></h1>
+    @foreach($todos as $todo)
 
-@endsection
+
+         {{ $todo->todo_user_id }}
+         {{ $todo->id }}   {{--タスクID--}}
+         {{ $todo->todo_user_id }} {{-- ユーザーID--}}
+         {{ $todo->title }} {{-- タイトル--}}
+         {{ $todo->description }} - 説明（nullable）
+         {{ $todo->due_date }} - 期限日時（nullable）
+       
+    @endforeach
+
+
 
