faa9a1af75fcb1fcc4820e7f182ee7542892f990
kouheigit <ookawakouhei777@gmail.com>
2025-11-18 14:46:40 +0900

    fix:index.blade.phpを修正

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,8 +2,7 @@
 @section('content')
     <h1>test</h1>
     <h1>Todoインデックスブレード</h1>
-    <p>{{ Auth::guard('todo')->user()->name }} さんのタスク</p>
-    <b>{{ Auth::guard('todo')->user()->name }}さんのタスク</b>
+   
 
 @endsection
 
