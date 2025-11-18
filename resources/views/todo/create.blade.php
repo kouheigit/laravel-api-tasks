df61bf8d2c51fd5185deffae73d0a239066ceb07
kouheigit <ookawakouhei777@gmail.com>
2025-11-19 07:31:06 +0900

    Add:createの実装

--- resources/views/todo/create.blade.php
+++ resources/views/todo/create.blade.php
@@ -1,4 +1,11 @@
 @extends('todo.layout')
 @section('content')
-  
+    <h1>TodoUser　ダッシュボード</h1>
+    <p>
+        {{Auth::guard('todo')->user()->name }}さんでログイン中
+    </p>
+    <form method="POST" action="{{route('todo.store')}}">
+        @csrf
+        <button type="submit">登録する</button>
+    </form>
 @endsection
