a3ec1bc6558bef65a0219d4f1e8bdfcf55deccd7
kouheigit <ookawakouhei777@gmail.com>
2025-11-18 14:45:54 +0900

    feat:index.blade.phpへの実装

--- resources/views/todo/index.blade.php
+++ resources/views/todo/index.blade.php
@@ -2,5 +2,8 @@
 @section('content')
     <h1>test</h1>
     <h1>Todoインデックスブレード</h1>
+    <p>{{ Auth::guard('todo')->user()->name }} さんのタスク</p>
+    <b>{{ Auth::guard('todo')->user()->name }}さんのタスク</b>
+
 @endsection
 
