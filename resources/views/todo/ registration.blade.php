d626eaa3a06e247654e4dffe676e32414579bbdc
kouheigit <ookawakouhei777@gmail.com>
2025-11-21 16:28:22 +0900

    feat:Resistration.blade.phpの実装

--- resources/views/todo/ registration.blade.php
+++ resources/views/todo/ registration.blade.php
@@ -1,42 +1,20 @@
 @extends('todo.layout')
 @section('content')
     <h1>TodoUser　ダッシュボード</h1>
-    <p>
-        {{Auth::guard('todo')->user()->name }}さんでログイン中
-    </p>
-    <form method="POST" action="{{route('todo.store')}}">
-        @csrf
-        <div>
-            <label>ステータス:</label><br>
-            <select name="todo_status_id">
-                @foreach($statuses as $status)
-                    <option value="{{$status->id }}">
-                        {{$status->label}}
-                    </option>
-                @endforeach
-            </select>
-        </div>
-
-        {{-- 優先度 --}}
-        <div>
-            <label>優先度:</label>
-            <select name="todo_priority_id">
-                @foreach($priorities as $priority)
-                    <option value="{{$priority->id }}">
-                        {{$priority->label}}
-                    </option>
-                @endforeach
-            </select>
-        </div>
         <div>
             <label>タイトル：</label><br>
-            <input type="text" name="title" value="{{ old('title') }}">
+            <input type="name" name="name" value="{{ old('name') }}">
         </div>
         <div>
-            <label>内容：</label>
+            <label>e-mail：</label>
             <br>
-            <textarea name="description">{{ old('description') }}</textarea>
+            <input type="email" name="email" value="{{ old('email') }}">
         </div>
+    <div>
+        <label>password：</label>
+        <br>
+        <input type="password" name="password" value="{{ old('password') }}">
+    </div>
         <button type="submit">登録する</button>
     </form>
 @endsection
