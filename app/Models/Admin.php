cd002411d4d76f82f9a61b045bdaafdfe8439309
kouheigit <ookawakouhei777@gmail.com>
2026-01-14 18:55:53 +0900

    feat:Adminモデルの追記

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,9 +2,30 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Admin extends Model
 {
-    //
+    use HasFactory;
+
+    protected $table = 'members';
+
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+    ];
+
+    protected $hidden = [
+        'password',
+        'remember_token',
+    ];
+
+    protected function casts(): array
+    {
+        return [
+            'password' => 'hashed',
+        ];
+    }
 }
