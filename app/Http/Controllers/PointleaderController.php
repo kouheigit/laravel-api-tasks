f4a0267deed13a3b1852a63a09ebea1420324b22
kouheigit <ookawakouhei777@gmail.com>
2026-01-15 18:44:42 +0900

    feat:PointleaderControllerの実装

--- app/Http/Controllers/PointleaderController.php
+++ app/Http/Controllers/PointleaderController.php
@@ -6,12 +6,12 @@ use Illuminate\Http\Request;
 
 class PointleaderController extends Controller
 {
-    //ポイント使用メソット
+    //ポイント使用メソット(member等が使用できる)
     public function use(Request $request)
     {
         //
     }
-    //ポイント使用メソット
+    //ポイント使用メソット(ポイント付与できるUserのみが利用できる)
     public function get(Request $request)
     {
         //
