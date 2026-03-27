baf5586dbda049a6394fea968e5de6ad82e6c49c
kouheigit <ookawakouhei777@gmail.com>
2026-03-27 23:46:37 +0900

    feat:StockManager

--- app/OrderExample/StockManager.php
+++ app/OrderExample/StockManager.php
@@ -1,6 +1,23 @@
 <?php
 
+class StockManager
+{
+    private int $stock = 0;
 
+    public function decreaseStock(int $quantity): void
+    {
+        $this->stock -=$quantity;
+
+        if($this->stock < 0) {
+            $this->stock = 0;
+            $this->markAsOutOfStock();
+        }
+    }
+    private function markAsOutOfStock(): void
+    {
+        // 在庫切れ処理
+    }
+}
 /*いい例
 class StockManager
 {
