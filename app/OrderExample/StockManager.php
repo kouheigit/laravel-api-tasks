0313c63b9cdac93f3c6156b252a44e5db1ee2ba6
kouheigit <ookawakouhei777@gmail.com>
2026-03-27 23:44:05 +0900

    StockManagerクラスの実装

---
+++ app/OrderExample/StockManager.php
@@ -0,0 +1,43 @@
+<?php
+
+
+/*いい例
+class StockManager
+{
+    private int $stock = 0;
+
+    public function decreaseStock(int $quantity): void
+    {
+        $this->stock -= $quantity;
+
+        if ($this->stock < 0) {
+            $this->stock = 0;
+            $this->markAsOutOfStock();
+        }
+    }
+
+    private function markAsOutOfStock(): void
+    {
+        // 在庫切れ処理
+    }
+}
+/*悪い例
+class MemoryStateManager
+{
+    private int $intValue01 = 0;
+
+    public function changeValue01(int $changeValue): void
+    {
+        $this->intValue01 -= $changeValue;
+
+        if ($this->intValue01 < 0) {
+            $this->intValue01 = 0;
+            $this->updateState02Flag();
+        }
+    }
+
+    private function updateState02Flag(): void
+    {
+        // 何してるかわからん
+    }
+}*/
