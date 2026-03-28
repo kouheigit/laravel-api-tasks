b663fdf0e486fd1d67afe07136af17df33867532
kouheigit <ookawakouhei777@gmail.com>
2026-03-28 11:21:22 +0900

    feat:UserService.phpの実装

--- app/OrderExample/UserService.php
+++ app/OrderExample/UserService.php
@@ -4,6 +4,12 @@
 class UserService
 {
     public function createUser(array $data){
+        //ネスト防止
+        $user = 0;
+        if (!$user) return;
+        if (!$user->isActive()) return;
+        if (!$user->hasSubscription()) return;
+        if (!$user->subscription->isValid()) return;
     }
     public function updateUser(int $id, array $data){
 
