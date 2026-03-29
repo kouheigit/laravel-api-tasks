acd0c84c844d71b7d7828ca80fef3d58e5f936f0
kouheigit <ookawakouhei777@gmail.com>
2026-03-29 20:38:02 +0900

    feat:HitPointの実装

--- app/OrderExample/HitPoint.php
+++ app/OrderExample/HitPoint.php
@@ -1,5 +1,46 @@
 <?php
 
-class HitPoint{
+class HitPoint
+{
+    private const MAX = 999;
 
+    public function __construct(private int $value) {}
+
+    public function damage(int $amount): self
+    {
+        return new self(max(0, $this->value - $amount));
+    }
+
+    public function heal(int $amount): self
+    {
+        return new self(min(self::MAX, $this->value + $amount));
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
 }
+/*
+ * class HitPoint
+{
+    private const MAX = 999;
+
+    public function __construct(private int $value) {}
+
+    public function damage(int $amount): self
+    {
+        return new self(max(0, $this->value - $amount));
+    }
+
+    public function heal(int $amount): self
+    {
+        return new self(min(self::MAX, $this->value + $amount));
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+}
+ */
