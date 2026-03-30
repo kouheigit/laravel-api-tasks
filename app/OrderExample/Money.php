d2e70f4318dc599b96947aa4b0763d685864bf78
kouheigit <ookawakouhei777@gmail.com>
2026-03-30 19:12:05 +0900

    feat:Moneyの実装

--- app/OrderExample/Money.php
+++ app/OrderExample/Money.php
@@ -1,1 +1,18 @@
 <?php
+
+final class Money
+{
+    public function __construct(
+        public readonly int $amount,
+        public readonly string $currency
+    ) {}
+
+    public function add(self $other): self
+    {
+        return new self(
+            $this->amount + $other->amount,
+            $this->currency
+        );
+    }
+}
+
