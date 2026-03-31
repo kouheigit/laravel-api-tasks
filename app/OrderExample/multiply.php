b7034b10c9e132fd14a75a16504f2323d8eb9eb5
kouheigit <ookawakouhei777@gmail.com>
2026-03-31 10:23:17 +0900

    Add:AttackPower.php

--- app/OrderExample/multiply.php
+++ app/OrderExample/multiply.php
@@ -1,1 +1,13 @@
 <?php
+
+public function multiplyByRate(float $rate): self
+{
+    if ($rate < 0) {
+        throw new InvalidArgumentException('倍率は0以上である必要があります。');
+    }
+
+    return new self(
+        (int) round($this->amount * $rate),
+        $this->currency
+    );
+}
