3318181de4c2c74c586dcdcd5770e74599a5b681
kouheigit <ookawakouhei777@gmail.com>
2026-03-31 12:53:43 +0900

    feat:AttackPowerの実装

--- app/OrderExample/AttackPower.php
+++ app/OrderExample/AttackPower.php
@@ -1,3 +1,56 @@
 <?php
-f
+final class AttackPower
+{
+    private const MIN = 0;
+
+    public function __construct(private int $value) {
+        if ($value < self::MIN) {
+            throw new InvalidArgumentException('攻撃力は0以上');
+        }
+    }
+
+    public function value(): int
+    {
+        return $this->value;
+    }
+
+    public function enhance(self $increment): self
+    {
+        return new self($this->value + $increment->value);
+    }
+
+    public function disable(): self
+    {
+        return new self(self::MIN);
+    }
+}
+
+/*
+/*
+ * 悪い例
+class AttackPower
+{
+    private const MIN = 0;
+
+    public int $value;
+
+    public function __construct(int $value)
+    {
+        if($value < self::MIN){
+            throw new \http\Exception\InvalidArgumentException();
+        }
+        $this->value = $value;
+    }
+
+    public function enhance(int $increment): void
+    {
+        $this->value += $increment;//←直接変更
+    }
+
+    public function disable(): void
+    {
+        $this->value = self::MIN;//←直接変更
+    }
+}*/
+
 
