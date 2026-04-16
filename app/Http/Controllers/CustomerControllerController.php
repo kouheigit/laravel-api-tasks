05af521e4bd607f8c33cf9ee1e82265ade7d10f8
kouheigit <ookawakouhei777@gmail.com>
2026-04-16 09:21:50 +0900

    Add:Customerブレードファイルの追加

---
+++ app/Http/Controllers/CustomerControllerController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CustomerController;
+use Illuminate\Http\Request;
+
+class CustomerControllerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CustomerController $customerController)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(CustomerController $customerController)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, CustomerController $customerController)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CustomerController $customerController)
+    {
+        //
+    }
+}
